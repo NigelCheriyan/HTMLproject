99b4240b2c1a3c36d553247b245b58eb9b36c3bf
Nigel Cheriyan <cheriyan.nigel@protonmail.com>
2023-10-26 11:18:34 -0400

    Can now append multiple things to a list

--- Interactivity.js
+++ Interactivity.js
@@ -1,5 +1,7 @@
-function addItem(e){
-    var userInput = document.getElementById("userInput").value;
-    document.getElementById("noItem").innerHTML = userInput
 
+function addItem(text){
+    let newTask = document.createElement("li");
+    let userInput = document.createTextNode(document.getElementById("userInput").value);
+    newTask.appendChild(userInput);
+    document.getElementById("List").appendChild(newTask);
 }
