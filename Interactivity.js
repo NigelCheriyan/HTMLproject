fc9c904c1cfea72ab474b09f48c18a02eb8dd95e
Nigel Cheriyan <cheriyan.nigel@protonmail.com>
2023-11-10 12:55:20 -0500

    Add delete button

--- Interactivity.js
+++ Interactivity.js
@@ -1,7 +1,36 @@
 
+//Create object oriented todolist
+//
+
 function addItem(text){
     let newTask = document.createElement("li");
     let userInput = document.createTextNode(document.getElementById("userInput").value);
+    let button = document.createElement('button')
+    button.textContent = ' X';
+    button.addEventListener('click', () => {document.getElementById("List").removeChild(newTask);});
     newTask.appendChild(userInput);
+    newTask.appendChild(button);
     document.getElementById("List").appendChild(newTask);
+
 }
+function deleteItem(text){
+    document.getElementById("List").removeChild(newTask);
+
+}
+/*class toDoItem{
+  constructor(item){
+    this.item = item;
+  }
+   newItem(
+      let newTask = document.createElement("li");
+      let userInput = document.createTextNode(this.item);
+      newTask.appendChild(userInput);
+      document.getElementById("List").appendChild(newTask);
+    )
+    deleteItem(
+      document.getElementById("List").removeChild(newTask);
+
+
+    )
+};
+*/
